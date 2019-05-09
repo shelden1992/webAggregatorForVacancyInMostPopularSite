b1864753b3682c8bcb087b15e4a002ccb08f0bbc
macuser <shelupets1992@gmail.com>
2019-05-09 14:42:57 +0300

    Revert "try to go on sdk_12"
    This reverts commit 3ff023ed

--- src/main/java/net/servletDatabase/filters/DatabaseFilter.java
+++ src/main/java/net/servletDatabase/filters/DatabaseFilter.java
@@ -5,7 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Properties;
 
 public class DatabaseFilter extends AbstractDatabaseFilter {
 
